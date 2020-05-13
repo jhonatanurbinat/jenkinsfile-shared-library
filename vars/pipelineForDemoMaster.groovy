aadf12e6bbf3c91bf57e99d249ec323b369495ab
manu756 <manu.franco@genial.ly>
2020-05-13 21:46:07 +0200

    Testing build, deliver and deploy

--- vars/pipelineForDemoMaster.groovy
+++ vars/pipelineForDemoMaster.groovy
@@ -8,12 +8,34 @@ def call(env){
             }
         }
         stages {
-            stage('First Test') {
+            stage('Build') {
+                steps {
+                    script {
+                        sh "docker build ${env.DOCKERFILE_LOCATION} -t ${env.DOCKER_IMAGE}:${env.VERSION}"
+                    }
+                }
+            }
+            stage('Deliver') {
+                steps {
+                    script {
+                        sh "docker push ${env.DOCKER_IMAGE}:${env.VERSION}"
+                    }
+                }
+            }
+            stage('Deploy') {
                 steps {
                     script {
                         withCredentials([file(credentialsId: 'kubeconfig', variable: 'kubeconfig')]) {
-                            sh "kubectl --kubeconfig ${kubeconfig} get pods"
+                            sh "kubectl --kubeconfig ${kubeconfig} apply -f manifests/deployment-${env.VERSION}.yaml"
                         }
+                    }
+                }
+            }
+            stage('Do Blue/Green') {
+                steps {
+                    script {
+                        input message: 'Do you want switch apps?', ok: 'Switch!'
+                        println "Here Blue/Green"
                     }
                 }
             }
